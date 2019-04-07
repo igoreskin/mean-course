d85ad998fbac4eaf9c4fe5abe4c76f51c5254257
Igor Eskin <eskinhd@gmail.com>
2019-04-07 17:01:58 -0400

    Local storage functionality added temporarily

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -26,11 +26,19 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
+    // this is temporary:
+    if (!localStorage.getItem('posts')) {
+      localStorage.setItem('posts', '[]')
+    }
+    // 
     const post: Post = {id: null, title: title, content: content};
     this.http.post<{message:string}>('http://localhost:3000/api/posts', post)
     .subscribe((responseData) => {
       console.log(responseData.message);
       this.posts.push(post);
+      // this is also temporary:
+      localStorage.setItem('posts', `${JSON.stringify(this.posts)}`);
+      // 
       this.postsUpdated.next([...this.posts]);
     })
   }
