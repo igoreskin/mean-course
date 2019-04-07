41313cc1c3ea4704efa03ecb7c931c86caf970fb
Igor Eskin <eskinhd@gmail.com>
2019-04-07 18:11:14 -0400

    Connected to database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://igoreskin:MongoDB20@cluster0-kbdpm.mongodb.net/test?retryWrites=true")
+.then(() => {
+    console.log('Connected to database');
+})
+.catch(() => {
+    console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +25,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
