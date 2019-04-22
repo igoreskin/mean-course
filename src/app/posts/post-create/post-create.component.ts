08a450ab7bc9c5e6ab2db65c44d666232097905e
Igor Eskin <eskinhd@gmail.com>
2019-04-22 16:14:00 -0400

    Edit functionality works

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -13,18 +13,20 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
+  post: Post;
   private mode = 'create';
   private postId: string;
-  private post: Post;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if(paramMap.has('postId')) {
+      if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData => {
+        this.post = {id: postData._id, title: postData.title, content: postData.content}
+       });
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -32,11 +34,16 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content)
+    }
+
     form.resetForm();
   }
 
