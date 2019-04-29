0761540277560982153027a507816f767441e5a8
Igor Eskin <eskinhd@gmail.com>
2019-04-29 17:41:57 -0400

    multer storage created on the backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const multer = require('multer');
+
 const Post = require('../models/post');
 
 const router = express.Router();
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  file: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);     // null means that there's no errors
+  }
+});
 
 router.post("", (req, res, next) => {
   const post = new Post({
